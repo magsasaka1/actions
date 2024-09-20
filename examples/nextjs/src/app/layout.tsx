6d2f78d4e782ae7d6189a44a8a5a9c8d2d14a442
RsdNoob <60094708+RsdNoob@users.noreply.github.com>
2024-09-20 11:37:53 +0800

    removed layout headers

--- examples/nextjs/src/app/layout.tsx
+++ examples/nextjs/src/app/layout.tsx
@@ -1,13 +1,7 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "magsasaka Blinks",
-  description: "magsasaka Blinks Adventure",
-};
 
 export default function RootLayout({
   children,
--- examples/nextjs/src/app/page.tsx
+++ examples/nextjs/src/app/page.tsx
@@ -1,12 +1,21 @@
+import Head from "next/head";
 import { IconBrandGithub } from "@tabler/icons-react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24">
-      <p>magsasaka Blinks</p>
-      <a href="https://github.com/magsasaka1/actions">
-        <IconBrandGithub stroke={2} />
-      </a>
-    </main>
+    <>
+      <Head>
+        {/* Add title and description for the homepage */}
+        <title>magsasaka Blinks</title>
+        <meta name="description" content="magsasaka Blinks Adventure" />
+      </Head>
+
+      <main className="flex min-h-screen flex-col items-center justify-start gap-4 p-24">
+        <p>magsasaka Blinks</p>
+        <a href="https://github.com/magsasaka1/actions">
+          <IconBrandGithub stroke={2} />
+        </a>
+      </main>
+    </>
   );
 }
